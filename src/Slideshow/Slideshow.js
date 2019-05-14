dd8fe175034933179c5bcc1dd0b229a9a52d7e65
Ben Lardie <benlardie@gmail.com>
2019-05-14 18:17:23 -0400

    fixed so logo only shows up on phone

--- src/Slideshow/Slideshow.js
+++ src/Slideshow/Slideshow.js
@@ -26,11 +26,9 @@ const api_key = process.env.REACT_APP_TMDB_API_KEY
 const url = `https://api.themoviedb.org/3/keyword/54169/movies?api_key=${api_key}&language=en-US&include_adult=false`
 
 const imgBaseUrl = 'https://image.tmdb.org/t/p/w1400_and_h450_face/'
-const posterBaseUrl = 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/'
 
 const Slideshow = () => {
     const [images, setImages] = useState(slideImages);
-    const [posters, setPosters] = useState(slideImages);
 
     useEffect(() => {
         axios.get(url).then(response => {
@@ -45,18 +43,6 @@ const Slideshow = () => {
         });
     }, []);
 
-    useEffect(() => {
-        axios.get(url).then(response => {
-            let posterImages = [{'url': logo, 'title': null }];
-            const results = response.data.results.slice(1, 4);
-            results.forEach(element => {
-                if (element.poster_path) {
-                    posterImages.push({'url':posterBaseUrl + element.poster_path, 'title': element.title});
-                }
-            });
-            setPosters(posterImages);
-        });
-    }, []);
 
     const slides = images.map((img, index) => (
         <div className="each-slide" key={index}>
@@ -65,13 +51,6 @@ const Slideshow = () => {
         </div>
     ));
 
-    const posterSlides = posters.map((img, index) => (
-        <div className="each-slide-poster" key={index}>
-            {/* <img src ={img.url} alt={img.title} /> */}
-            <div style={{'backgroundImage': `url(${img.url})`}} />
-            <h2 className='slideshow-title'>{img.title}</h2>
-        </div>
-    ));
 
     return (
         <>
@@ -81,9 +60,7 @@ const Slideshow = () => {
                 </Slide>
             </div>
             <div className="poster-slides">
-                <Slide {...properties}>
-                    {posterSlides}
-                </Slide>
+                <img src={logo} alt="buddflix-logo" />
             </div>
         </>
     )
